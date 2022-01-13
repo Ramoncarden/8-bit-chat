689188658a0ffabcc14b1c0c93c9453d84cf7d01
Ramon Cardenas <ramoncarden@gmail.com>
2022-01-12 21:25:18 -0600

    Added message display to chat container and applied message stylings including username, message content, date time. Adjusted content layout for message display.

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ChatContext from '../Context/ChatContext';
 import Banner from './Banner';
 import MessageInput from './MessageInput';
 
 const ChatRoom = () => {
+  const { formData, setFormData } = useContext(ChatContext);
+  const date = new Date();
+  const time = new Date().toLocaleString();
   return (
-    <div className='bg-orange-100 h-full flex flex-col sm:w-full h-screen relative'>
+    <div className='flex flex-col bg-orange-100 h-full sm:w-full h-screen relative'>
       <Banner />
+      <div className='absolute bottom-20 left-3 leading-6'>
+        <p className='inline-block text-gray-700 tracking-wide'>
+          <span className='float-left inline-block italic font-ps2 text-emerald-700 text-xs pt-1 tracking-wide mr-3'>
+            RegisteredChatUser
+          </span>
+          {formData.content}
+          <span className='text-xs italic ml-2 text-gray-500'> ({time})</span>
+        </p>
+      </div>
       <MessageInput />
     </div>
   );
